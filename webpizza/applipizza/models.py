e56c5f79bb1cf7e06ae7958471dde853e879e69c
Nathan GILBERT <gilbertnathan123@icloud.com>
2023-09-22 11:19:03 +0200

    push fou

--- webpizza/applipizza/models.py
+++ webpizza/applipizza/models.py
@@ -4,29 +4,49 @@ from django.db import models
 
 class Ingredient(models.Model):
     
-    #clé primaire avec auto incrémentation
+    # idIngredient est une clé primaire, n auto-incrémentée => AutoField
     idIngredient = models.AutoField(primary_key=True)
-
-    #nom de l'ingrédient
-    nomIngredient = models.CharField(max_length=50, verbose_name= "Nom de l'ingrédient")
-
-    #methode to string
+    
+    #nomIngredient est une chaines de caractères => CharField
+    nomIngredient = models.CharField(max_length=50, verbose_name='le nom de l\'ingrédient')
+    
+    # une méthode de type "toString"
     def __str__(self) -> str:
         return self.nomIngredient
-
-class Pizza(models.Model) :
-
-    #clé primaire avec auto incrémentation
+    
+class Pizza(models.Model):
+    # idPizza est une clé primaire, n auto-incrémentée => AutoField
     idPizza = models.AutoField(primary_key=True)
-
-    #nom de la pizza
-    nomPizza = models.CharField(max_length=50, verbose_name= "Nom de la pizza")
-
-    #prix de la pizza
-    prixPizza = models.DecimalField(max_digits=4, decimal_places=2, verbose_name= "Prix de la pizza")
-
-    #tosring
-    def __str__(self):
-        return self.nomPizza
-
-
+    
+    #nomPizza est une chaines de caractères => CharField
+    nomPizza = models.CharField(max_length=50, verbose_name='le nom de la pizza')
+    
+    #prixPizza est un nombre décimal => DecimalField
+    prixPizza = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='le prix de la pizza')
+    
+    #une méthode de type "toString"
+    def __str__(self) -> str:
+        return 'pizza' + self.nomPizza + '(prix : ' + str(self.prixPizza) + '€)'
+    
+class Composition(models.Model) : 
+    
+    # la classe Meta qui gère l'unicité du couple de clés étrangères
+    class Meta:
+        unique_together = (('ingredient', 'pizza'),) # le nom des chaps clés étrangères
+        
+    # idComposition est une clé primaire, n auto-incrémentée => AutoField
+    idComposition = models.AutoField(primary_key=True)
+    
+    # les deux champs clés étrangères, dont les noms correspondent
+    # aux classe respectives, en minuscules 
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    
+    # l'autre champ, chaîne de caractères 
+    quantite = models.CharField(max_length=100, verbose_name='la quantité ')
+    
+    # une méthode de type "toString"
+    def __str__(self) -> str:
+        ing = self.ingredient
+        piz = self.pizza
+        return ing.nomIngredient + ' fait partie de la pizza ' + piz.nomPizza + '( quantité ' + self.quantite + ')'
--- webpizza/applipizza/views.py
+++ webpizza/applipizza/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from applipizza.models import Pizza
+from applipizza.models import Ingredient
+
+
+def pizzas(request) : 
+
+    lesPizzas = Pizza.objects.all()
+
+    return render(
+        request,
+        'applipizza/pizza.html',
+        {'pizzas' : lesPizzas}
+    )
+
+def ingredients(request) :
+
+    lesIngredients = Ingredient.objects.all()
+
+    return render(
+        request,
+        'applipizza/ingredients.html',
+        {'ingredient' : lesIngredients}
+    ) 
